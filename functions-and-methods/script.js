b7eeb5ccac8b0afb2ffe290f38774a609d0fa479
Fatima Qaisar <fatima79176985@gmail.com>
2026-01-23 16:21:54 +0500

    Functions and Methods

--- functions-and-methods/script.js
+++ functions-and-methods/script.js
@@ -62,10 +62,50 @@ const countVow=(str)=>{
 
 let arr =[1,2,3,4,5];                     //forEach will take each element of array and pass it to function as value
 arr.forEach(function printVal(val){      //value at each ndex
-      console.log(val);
+      console.log(val);                       
 });
 
+//forEach is a higher order function
 let arr2=["Lahore", "Multan", "Faisalabad"];
 arr2.forEach((val, i, arr)=>{
     console.log(val.toUpperCase(), i, arr);
 });
+
+//Practice
+let numArray=[2,3,4,5,6,7];
+console.log("\nforEach Practice")
+numArray.forEach((val)=>{
+    console.log(val*val); //val**2
+});
+let calcSquare =(num)=>{
+    console.log(num*num);
+};
+numArray.forEach(calcSquare); //Also a valid way to write call back function. pass the variable that contains the function definition
+
+//finding max value
+console.log("\nFinding Maximum Value");
+let arr3=[8, 6,2,77,3];
+
+const maxValue=arr3.reduce((prev,curr)=>{
+    return prev>curr? prev:curr;
+});
+console.log(maxValue);
+
+//Filter method
+let marks=[87, 93, 67, 56,76, 95];
+let toppers=marks.filter((val)=>{ 
+    return val>90;
+});
+console.log(toppers);
+
+//Reduce Method
+let n= prompt("Enter a number: ");
+let arr4=[];
+for (let i=1; i<=n ; i++){
+    arr4[i-1]=i;
+}
+console.log(arr4);
+let factorial=arr4.reduce((prev, curr)=>{ 
+    return prev*curr;
+});
+console.log(factorial);
