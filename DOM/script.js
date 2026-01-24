7df2f9f9e0de008794287bc4e0bc034d484164ac
Fatima Qaisar <fatima79176985@gmail.com>
2026-01-24 23:54:38 +0500

    Document Object Model

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+console.log("hello");
+window.console.log("hello");
+console.log(window);
+console.dir(window); //both can be used to print the window object
+
+console.log(document); //to access html i javascript   //window is global object
+console.dir(window.document); //to see properties of document
+//console.log ->prints object as an elemnet. we can print the DOM elements directly -> html code 
+//console.dir ->to print properties and methods of special objects. it is also a part of the window object. ->object gets printed
+
+console.log(document.body);
+console.dir(document.body);
+
